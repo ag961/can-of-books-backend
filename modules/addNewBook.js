bb936e3a9de760005dc323ed9d9bda18d8e70f7a
Ayrat <gimranov45@gmail.com>
2021-08-22 09:52:36 +0300

    lab13 in progress

--- modules/addNewBook.js
+++ modules/addNewBook.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const BookModel = require('../models/books.js');
 
+
 let addNewBook = async (obj) => {
   let newBook = new BookModel(obj);
   await newBook.save();
--- server.js
+++ server.js
@@ -6,13 +6,21 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected');
+});
+
 const clear = require('./modules/clearDB.js');
 const seed = require('./modules/seed.js');
 const getBooks = require('./modules/getBooks');
+const createBook = require('./modules/createBook');
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -23,6 +31,8 @@ app.get('/books', getBooks) ;
 app.get('/clear', clear);
 
 app.get('/seed', seed);
+
+app.post('/books', createBook);
 
 mongoose.connect('mongodb://127.0.0.1:27017/books', {
   useNewUrlParser: true,
